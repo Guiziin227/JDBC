c1dad8188bd5fcd6b149f53f02dcc80874f312c2
Guiziin227 <gh63388@gmail.com>
2025-04-20 19:12:31 -0300

    add database query execution in main method

--- src/aplication/Main.java
+++ src/aplication/Main.java
@@ -3,11 +3,31 @@ package aplication;
 import db.DB;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
 
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        Connection conn = null;
+
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+
+            st = conn.createStatement();//instancia o objeto st
+
+            rs = st.executeQuery("SELECT * FROM department");//executa a query (comando sql)
+
+            while (rs.next()) {//enquanto houver um proximo resultado
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));//get e o nome da coluna
+            }
+
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 }
