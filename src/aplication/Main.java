0dee2eb692d2de5b8aaa19c00350c9a10ccd2713
Guiziin227 <gh63388@gmail.com>
2025-04-20 19:30:14 -0300

    add generated key retrieval after seller insertion in main method

--- src/aplication/Main.java
+++ src/aplication/Main.java
@@ -25,7 +25,7 @@ public class Main {
 
 
             pst = conn.prepareStatement("INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentID) "
-                    + " VALUES (?,?,?,?,?)");//prepara a query
+                    + " VALUES (?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);//prepara a query
 
             pst.setString(1, "Guilherme");//seta o primeiro parametro
             pst.setString(2, "gh630@gmail.com");//seta o segundo parametro
@@ -34,7 +34,15 @@ public class Main {
             pst.setInt(5, 4);//seta o quinto parametro
 
             int rowsAffected = pst.executeUpdate();
-            System.out.println("Rows affected: " + rowsAffected);//imprime o numero de linhas afetadas
+            if (rowsAffected > 0) {
+                ResultSet rs1 = pst.getGeneratedKeys();
+                while (rs1.next()) {
+                    int id = rs1.getInt(1);
+                    System.out.println("Done! Id: " + id);
+                }
+            } else {
+                System.out.println("No rows affected!");
+            }
 
             rs = st.executeQuery("SELECT * FROM department");//executa a query (comando sql)
             while (rs.next()) {//enquanto houver um proximo resultado
